2ff6b80f9a228b9dfd145218bd9769402a9c708d
Pawanjot-Randhawa <pawanjot2004@gmail.com>
2025-03-08 22:00:23 -0500

    Added all labels relevant to InstructionsPanel

--- src/project/InstructionsPanel.java
+++ src/project/InstructionsPanel.java
@@ -1,5 +1,19 @@
 package project;
 
+import javax.swing.*;
+
 public class InstructionsPanel {
+
+    JLabel I1 = new JLabel("The Objective in this game is to kill the red enemy by shooting them with your bullets.");
+    JLabel I2 = new JLabel("While atempting to kill the enemy, you must also avoid their bullet in order to stay alive.");
+    JLabel I3 = new JLabel("Bullet can not destroy each other, so dodging is the only way to avoid damage.");
+    JLabel I4 = new JLabel("In terms of damage, all enemy bullet deal 20 damage while the Player has only 100 health.");
+    JLabel I5 = new JLabel("All player bullets deal 10 damage, and enemy health depends on difficulty.");
+    JLabel I6 = new JLabel("The amount of enemy bullet also change with diffuclty. The infomration is listed below:");
+    JLabel I7 = new JLabel("Easy: 50 Health, 1 Bullet");
+    JLabel I8 = new JLabel("Meduim: 70 Health, 2 Bullets");
+    JLabel I9 = new JLabel("Hard: 100 Health, 3 Bullets");
+    JLabel I10 = new JLabel("Impossible: 130 Health, 6 Bullets");
+    JLabel I11 = new JLabel("You can use either WASD or arrows keys to move, and hit the SPACE BAR to shoot");
 
 }
