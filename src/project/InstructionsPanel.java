0ea3c42d4d779bf9844d21dbf7ba808914fdb372
Pawanjot-Randhawa <pawanjot2004@gmail.com>
2025-03-09 22:25:23 -0400

    Configured Label Setting and added them to the Panel.

--- src/project/InstructionsPanel.java
+++ src/project/InstructionsPanel.java
@@ -1,8 +1,9 @@
 package project;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class InstructionsPanel {
+public class InstructionsPanel extends JPanel{
 
     JLabel I1 = new JLabel("The Objective in this game is to kill the red enemy by shooting them with your bullets.");
     JLabel I2 = new JLabel("While atempting to kill the enemy, you must also avoid their bullet in order to stay alive.");
@@ -16,4 +17,59 @@ public class InstructionsPanel {
     JLabel I10 = new JLabel("Impossible: 130 Health, 6 Bullets");
     JLabel I11 = new JLabel("You can use either WASD or arrows keys to move, and hit the SPACE BAR to shoot");
 
+
+
+    public InstructionsPanel() {
+        //settings
+        this.setBounds(0, 0, 1000, 950);
+        this.setBackground(Color.gray);
+        this.setLayout(null);
+
+        //label settings
+        I1.setFont(new Font("Times New Roman", Font.PLAIN, 23));//sets the font
+        I1.setBounds(50, 100, 900, 50);//setting the size and location
+
+        I2.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I2.setBounds(50, 140, 900, 50);
+
+        I3.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I3.setBounds(50, 180, 900, 50);
+
+        I4.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I4.setBounds(50, 220, 900, 50);
+
+        I5.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I5.setBounds(50, 260, 900, 50);
+
+        I6.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I6.setBounds(50, 300, 900, 50);
+
+        I7.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I7.setBounds(50, 350, 900, 50);
+
+        I8.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I8.setBounds(50, 400, 900, 50);
+
+        I9.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I9.setBounds(50, 450, 900, 50);
+
+        I10.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I10.setBounds(50, 500, 900, 50);
+
+        I11.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+        I11.setBounds(50, 700, 900, 50);
+
+        //adding the labels
+        this.add(I1);
+        this.add(I2);
+        this.add(I3);
+        this.add(I4);
+        this.add(I5);
+        this.add(I6);
+        this.add(I7);
+        this.add(I8);
+        this.add(I9);
+        this.add(I10);
+        this.add(I11);
+    }
 }
